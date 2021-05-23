654d778fa63ab0d1e90ef9640a3ca78e26d8459a
Vivek Singh <svivekkumar@vmware.com>
2021-05-23 17:35:42 +0530

    Add a new func to the service
    Signed-off-by: Vivek Singh <svivekkumar@vmware.com>

--- greeter_client/main.go
+++ greeter_client/main.go
@@ -37,4 +37,12 @@ func main() {
 		log.Fatalf("could not greet: %v", err)
 	}
 	log.Printf("Greeting: %s", r.GetMessage())
+
+	// call another func SayHelloAgain
+	r, err = c.SayHelloAgain(ctx, &pb.HelloRequest{Name: name})
+	if err != nil {
+		log.Fatalf("could not greet: %v", err)
+	}
+	log.Printf("Greeting: %s", r.GetMessage())
+
 }
